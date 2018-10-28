23636051f7d98adeac19e493fb5dfdf98caa103a
xuyao <xuyao0@foxmail.com>
2018-10-28 18:56:37 +0800

    clean

--- client/src/main/java/com/xuyao/startup/ClientApplication.java
+++ client/src/main/java/com/xuyao/startup/ClientApplication.java
@@ -1,7 +1,6 @@
 package com.xuyao.startup;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -10,7 +9,7 @@ import org.springframework.context.annotation.ComponentScan;
 @EnableFeignClients(basePackages={"com.xuyao.feign"})
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableAutoConfiguration
+//@EnableAutoConfiguration
 @ComponentScan(basePackages = "com.xuyao")
 public class ClientApplication {
 
